4a56736d426aaef558527bb8f0fc378df3f209f0
yotubarail <shota_t@i.softbank.jp>
2022-01-23 15:06:21 +0900

    vue-router導入

---
+++ src/components/swiftevent/Swiftevent.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <main>
+      <section id="aboutSwiftevent">
+        <img src="../../img/top.jpg" alt="Swiftevent">
+        <h1>What's Swifrevent</h1>
+        <p>多くのイベントが掲載されるconnpassに対応し、イベント参加をSwiftにするアプリ</p>
+        <p>それがSwifteventです！！</p>
+        <div class="announcement">
+          <p>SwifteventにはSwiftUIを使用しました。</p>
+          <p class="iOS14">iOS14に対応しました！これからも「Swiftevent」よろしくお願い致します。</p>
+        </div>
+      </section>
+      <section id="feature">
+        <h2>Swifteventの3つの特徴</h2>
+        <div class="content">
+          <h3>勉強会を探せる！</h3>
+          <div class="detail">
+            <div class="text">
+              <p>connpass上で公開されている勉強会を探し出せる</p>
+            </div>
+            <div class="image">
+              <img src="../../img/image1.png" alt="検索画面">
+            </div>
+          </div>
+        </div>
+        <div class="content">
+          <h3>自分の参加するイベントが<br class="pc-none">わかる！</h3>
+          <div class="detail">
+            <div class="text">
+              <p>参加申し込みをしたイベントを最新順に10件まで表示</p>
+            </div>
+            <div class="image">
+              <img src="../../img/image2.png" alt="参加するイベント">
+            </div>
+          </div>
+        </div>
+        <div class="content">
+          <h3>ルート表示で会場まで<br class="pc-none">スムーズに向かおう！</h3>
+          <div class="detail">
+            <div class="text">
+              <p>地図画面を開くことで自動で現在地から会場までのルートを表示</p>
+            </div>
+            <div class="image">
+              <img src="../../img/image3.png" alt="地図画面">
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="download">
+        <a href="https://apps.apple.com/jp/app/swiftevent/id1527043419?mt=8" style="display:inline-block;overflow:hidden;background:url(https://linkmaker.itunes.apple.com/ja-jp/badge-lrg.svg?releaseDate=2020-08-13&kind=iossoftware&bubble=ios_apps) no-repeat;width:135px;height:40px;"></a>
+        <p>iOS13以上対応</p>
+      </section>
+    </main>
+    <footer>
+      <ul>
+        <li><a href="https://yotubarail.work/Swiftevent">プライバシーポリシー</a></li>
+        <li><a href="https://yotubarail.work/contact">コンタクト</a></li>
+      </ul>
+      <small>&copy; 2020 Shota Takinami </small>
+    </footer>
+  </div>
+</template>
